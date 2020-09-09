641c0736313a07855878b603e4a9d41d1f97f6c4
Jordan Cooper <jordan.cooper3000@gmail.com>
2020-09-09 05:17:29 -0500

    update python and headless chrome_bin

--- app/pyscraper.py
+++ app/pyscraper.py
@@ -8,12 +8,13 @@ def getInfo():
     This function collects data from ja-mis and return market prices
     """""
 
-    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
     CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
+    chrome_options.add_argument('headless')
+    chrome_options.binary_location = chrome_bin
     driver = webdriver.Chrome(
         executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     # marketData=[]
