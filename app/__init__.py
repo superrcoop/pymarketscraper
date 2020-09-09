99c2d9e501e456db562af083fbe9f2f77ec7dcd1
Jordan Cooper <jordan.cooper3000@gmail.com>
2020-09-09 04:41:06 -0500

    update procfile and _init_

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.pyscraper import getInfo
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 
 @app.route('/api/v1/marketInfo/prices/fruits', methods=['GET'])
@@ -13,3 +14,7 @@ def home():
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
