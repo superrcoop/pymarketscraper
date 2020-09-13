00718ae2114e77bac42c5573b5d84b3b10aafe7b
Jordan Cooper <jordan.cooper3000@gmail.com>
2020-09-13 13:51:48 -0500

    get all market prices: single route

--- app/pyscraper.py
+++ app/pyscraper.py
@@ -41,7 +41,8 @@ def get_all_prices():
     content = driver.page_source
     marketInfo = BeautifulSoup(content, features="html.parser")
     marketInfoDate = marketInfo.find(id="ctl00_ContentPlaceHolder1_weekending")
-    json_array = get_fruits_prices()
+    json_array = get_fruits_prices() + get_crops_prices() + get_herbs_prices() + \
+        get_legumes_prices() + get_veg_prices()
     if not json_array:
         json_obj = {'status': '404', 'message': 'not found',
                     'request': 'GET /api/v1/marketInfo/prices', 'data': []}
@@ -57,17 +58,108 @@ def get_fruits_prices():
     This function collects data from ja-mis and return market prices
     """""
     driver.get("http://www.ja-mis.com/Companionsite/home.aspx")
-    element = driver.find_element_by_id(
-        'ctl00_ContentPlaceHolder1_ddl_CategoryName')
-    select = Select(element)
-    select.select_by_visible_text('Fruits')
-    WebDriverWait(driver, 1).until(EC.staleness_of(element))
     content = driver.page_source
     marketInfo = BeautifulSoup(content, features="html.parser")
     json_array = []
 
     for row in marketInfo.find(id="hor-minimalist-b").tbody.find_all('tr'):
         json_data = {'Item': '', 'Type': '', 'FarmGate': '', 'Category': 'Fruits',
+                     'Municipal': '', 'Wholesale': '', 'Retail': ''}
+        if row.get_text():
+            for idx, td in enumerate(row.find_all('td')):
+                json_data[list(json_data)[idx]] = td.get_text()
+            json_array.append(json_data)
+    return json_array
+
+
+def get_herbs_prices():
+    """""
+    This function collects data from ja-mis and return market prices
+    """""
+    driver.get("http://www.ja-mis.com/Companionsite/home.aspx")
+    element = driver.find_element_by_id(
+        'ctl00_ContentPlaceHolder1_ddl_CategoryName')
+    select = Select(element)
+    select.select_by_index(1)
+    WebDriverWait(driver, 10).until(EC.staleness_of(element))
+    content = driver.page_source
+    marketInfo = BeautifulSoup(content, features="html.parser")
+    json_array = []
+
+    for row in marketInfo.find(id="hor-minimalist-b").tbody.find_all('tr'):
+        json_data = {'Item': '', 'Type': '', 'FarmGate': '', 'Category': 'Herbs & Spices',
+                     'Municipal': '', 'Wholesale': '', 'Retail': ''}
+        if row.get_text():
+            for idx, td in enumerate(row.find_all('td')):
+                json_data[list(json_data)[idx]] = td.get_text()
+            json_array.append(json_data)
+    return json_array
+
+
+def get_legumes_prices():
+    """""
+    This function collects data from ja-mis and return market prices
+    """""
+    driver.get("http://www.ja-mis.com/Companionsite/home.aspx")
+    element = driver.find_element_by_id(
+        'ctl00_ContentPlaceHolder1_ddl_CategoryName')
+    select = Select(element)
+    select.select_by_index(2)
+    WebDriverWait(driver, 10).until(EC.staleness_of(element))
+    content = driver.page_source
+    marketInfo = BeautifulSoup(content, features="html.parser")
+    json_array = []
+
+    for row in marketInfo.find(id="hor-minimalist-b").tbody.find_all('tr'):
+        json_data = {'Item': '', 'Type': '', 'FarmGate': '', 'Category': 'Legumes',
+                     'Municipal': '', 'Wholesale': '', 'Retail': ''}
+        if row.get_text():
+            for idx, td in enumerate(row.find_all('td')):
+                json_data[list(json_data)[idx]] = td.get_text()
+            json_array.append(json_data)
+    return json_array
+
+
+def get_crops_prices():
+    """""
+    This function collects data from ja-mis and return market prices
+    """""
+    driver.get("http://www.ja-mis.com/Companionsite/home.aspx")
+    element = driver.find_element_by_id(
+        'ctl00_ContentPlaceHolder1_ddl_CategoryName')
+    select = Select(element)
+    select.select_by_index(3)
+    WebDriverWait(driver, 10).until(EC.staleness_of(element))
+    content = driver.page_source
+    marketInfo = BeautifulSoup(content, features="html.parser")
+    json_array = []
+
+    for row in marketInfo.find(id="hor-minimalist-b").tbody.find_all('tr'):
+        json_data = {'Item': '', 'Type': '', 'FarmGate': '', 'Category': 'Root Crops',
+                     'Municipal': '', 'Wholesale': '', 'Retail': ''}
+        if row.get_text():
+            for idx, td in enumerate(row.find_all('td')):
+                json_data[list(json_data)[idx]] = td.get_text()
+            json_array.append(json_data)
+    return json_array
+
+
+def get_veg_prices():
+    """""
+    This function collects data from ja-mis and return market prices
+    """""
+    driver.get("http://www.ja-mis.com/Companionsite/home.aspx")
+    element = driver.find_element_by_id(
+        'ctl00_ContentPlaceHolder1_ddl_CategoryName')
+    select = Select(element)
+    select.select_by_index(4)
+    WebDriverWait(driver, 10).until(EC.staleness_of(element))
+    content = driver.page_source
+    marketInfo = BeautifulSoup(content, features="html.parser")
+    json_array = []
+
+    for row in marketInfo.find(id="hor-minimalist-b").tbody.find_all('tr'):
+        json_data = {'Item': '', 'Type': '', 'FarmGate': '', 'Category': 'Vegetables',
                      'Municipal': '', 'Wholesale': '', 'Retail': ''}
         if row.get_text():
             for idx, td in enumerate(row.find_all('td')):
